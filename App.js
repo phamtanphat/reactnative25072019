3dc1ca36ff62c67e78d802de34a018bad7f6d1b9
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-08-08 21:37:13 +0700

    a

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Design from './components/Design';
 
 export default class App extends Component {
   render() {
     return (
-      <View>
-        <Text>Hello React native</Text>
-      </View>
+      <Design/>
     );
   }
 }
