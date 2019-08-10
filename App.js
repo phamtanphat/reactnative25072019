153560bf0afd0fc292dece41d69daae27cd65594
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-08-10 20:01:15 +0700

    a

--- App.js
+++ App.js
@@ -2,10 +2,19 @@ import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import Design from './components/Design';
 
+const word = {en : "One" , vn : "Một" , isMemorized : true}
+
+//set color cho word.vn 
+// isMemorized : true = green
+// isMemorized : false : red
 export default class App extends Component {
   render() {
+  
     return (
-      <Design/>
+        <View style={{flex : 1 , flexDirection : "row" , justifyContent : "space-around" ,alignItems : "center"}}>
+          <Text style={{color : "green" , fontSize : 20}}>{word.en}</Text>
+          <Text style={{color : "red" , fontSize : 20}}>{word.vn}</Text>
+        </View>
     );
   }
 }
---
+++ components/Word.js
@@ -0,0 +1,12 @@
+import React, { PureComponent } from 'react'
+import { Text, View } from 'react-native'
+
+export default class Word extends PureComponent {
+    render() {
+        return (
+            <View>
+                <Text> textInComponent </Text>
+            </View>
+        )
+    }
+}
