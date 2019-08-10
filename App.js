9085aac50eaf2bb2ed4e921db1fed2314e600035
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-08-10 21:30:02 +0700

    a

--- App.js
+++ App.js
@@ -3,19 +3,24 @@ import {Platform, StyleSheet, Text, View} from 'react-native';
 import Word from './components/Word';
 import HandleArray from './components/HandleArray';
 
-//  const words = [
-//   {id : "a1" , en : "One" , vn : "Một" , isMemorized : true},
-//   {id : "a2" , en : "Two" , vn : "Hai" , isMemorized : false},
-//   {id : "a3" , en : "Three" , vn : "Ba" , isMemorized : false},
-//   {id : "a4" , en : "Four" , vn : "Bốn" , isMemorized : true},
-
-// ]
+ const words = [
+  {id : "a1" , en : "One" , vn : "Một" , isMemorized : true},
+  {id : "a2" , en : "Two" , vn : "Hai" , isMemorized : false},
+  {id : "a3" , en : "Three" , vn : "Ba" , isMemorized : false},
+  {id : "a4" , en : "Four" , vn : "Bốn" , isMemorized : true},
+]
 
 export default class App extends Component {
   render() {
     return (
         <View style={{flex : 1 }}>
-            <HandleArray/>
+            {words.map(function(word) {
+                return <Word 
+                          key={word.id} 
+                          en={word.en} 
+                          vn={word.vn} 
+                          isMemorized={word.isMemorized}/>
+            })}
         </View>
     );
   }
