14000bfd8e9dc13189044ad2f93a8c2060d6553d
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-08-13 20:44:46 +0700

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,12 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import Word from './components/Word';
-import HandleArray from './components/HandleArray';
-import Box from './components/Box';
- const words = [
-  {id : "a1" , en : "One" , vn : "Một" , isMemorized : true},
-  {id : "a2" , en : "Two" , vn : "Hai" , isMemorized : false},
-  {id : "a3" , en : "Three" , vn : "Ba" , isMemorized : false},
-  {id : "a4" , en : "Four" , vn : "Bốn" , isMemorized : true},
-]
+import List from './components/List';
+
 
 export default class App extends Component {
   render() {
     return (
-       <Box/>
+        <List/>
     );
   }
 }
