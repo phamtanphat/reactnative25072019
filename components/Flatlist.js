5b2aed45f6e9a7ede74dabf38e4b8327569eb5e2
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-08-15 20:05:41 +0700

    Update Flatlist.js

--- components/Flatlist.js
+++ components/Flatlist.js
@@ -1,11 +1,29 @@
 import React, { PureComponent } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View , FlatList , Dimensions} from 'react-native'
 
 export default class MyFlatlist extends PureComponent {
+    constructor(props){
+        super(props)
+        this.state = {
+            words : [
+                {id : "a1" , en : "One" , vn : "Một" , isMemorized : true},
+                {id : "a2" , en : "Two" , vn : "Hai" , isMemorized : false},
+                {id : "a3" , en : "Three" , vn : "Ba" , isMemorized : false},
+                {id : "a4" , en : "Four" , vn : "Bốn" , isMemorized : true},
+                {id : "a5" , en : "Five" , vn : "Năm" , isMemorized : true},
+                {id : "a6" , en : "Six" , vn : "Sáu" , isMemorized : true},
+            ]
+        }
+    }
     render() {
         return (
-            <View>
-                <Text> textInComponent </Text>
+            <View style={{flex : 1}}>
+                <FlatList
+                    data={this.state.words}
+                    keyExtractor={item => item.id}
+                    extraData={this.state}
+                    renderItem={({item , index}) => }
+                />
             </View>
         )
     }
