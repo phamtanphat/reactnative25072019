a8a78d00ff9f8003d61a9cb4b309ba9eb8fc053a
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-08-15 20:14:58 +0700

    Update Flatlist.js

--- components/Flatlist.js
+++ components/Flatlist.js
@@ -1,6 +1,9 @@
 import React, { PureComponent } from 'react'
-import { Text, View , FlatList , Dimensions} from 'react-native'
+import { Text, View , FlatList , Dimensions ,  TouchableOpacity} from 'react-native'
 
+
+
+const DeviceHeight = Dimensions.get('window').height
 export default class MyFlatlist extends PureComponent {
     constructor(props){
         super(props)
@@ -15,6 +18,32 @@ export default class MyFlatlist extends PureComponent {
             ]
         }
     }
+    getWordItem(word){
+        return (   
+            <View 
+                style={{ flexDirection : 'column' , height : DeviceHeight * 0.2 , margin : 10 , backgroundColor : 'gainsboro' , padding : 10 , borderRadius : 5}}>
+                <View style={{flexDirection : 'row' , justifyContent : 'space-around' , paddingBottom : 5}}>
+                    <Text style={{fontSize : 30 , color :  'green' }}>{word.en}</Text>
+                    <Text 
+                        style={{fontSize : 30 , color :  'red' }}>
+                            {word.isMemorized ? "----" : word.vn}
+                    </Text>
+                </View>
+                <View style={{flexDirection : 'row' , justifyContent : 'space-around' , paddingBottom : 5}}>
+                        <TouchableOpacity
+                            style={{backgroundColor : word.isMemorized ? "green" : "red" , padding : 10 , borderRadius : 5}}
+                        >
+                            <Text style={{fontSize : 20 , color : 'white'}}>{word.isMemorized ? "Forgot" : "isMemorized"}</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity  
+                            style={{backgroundColor : "orange" , padding : 10 , borderRadius : 5}}
+                        >
+                            <Text style={{fontSize : 25 , color :  'white' }}>Remove</Text>
+                        </TouchableOpacity>
+                </View>
+            </View>
+        )
+    }
     render() {
         return (
             <View style={{flex : 1}}>
@@ -22,7 +51,7 @@ export default class MyFlatlist extends PureComponent {
                     data={this.state.words}
                     keyExtractor={item => item.id}
                     extraData={this.state}
-                    renderItem={({item , index}) => }
+                    renderItem={({item , index}) => this.getWordItem(item)}
                 />
             </View>
         )
