b73ee73dfa50e25c669e345b289f103929436ec2
Phạm Tấn Phát <22995627+phamtanphat@users.noreply.github.com>
2019-08-15 19:54:21 +0700

    Create MyFlatilist.js

---
+++ components/MyFlatilist.js
@@ -0,0 +1,24 @@
+import React, { PureComponent } from 'react'
+import { Text, View } from 'react-native'
+
+export default class MyFlatlist extends PureComponent {
+    render() {
+        return (
+            <View>
+                <Text> textInComponent </Text>
+            </View>
+        )import React, { PureComponent } from 'react'
+import { Text, View } from 'react-native'
+
+export default class MyFlatlist extends PureComponent {
+    render() {
+        return (
+            <View>
+                <Text> textInComponent </Text>
+            </View>
+        )
+    }
+}
+
+    }
+}
