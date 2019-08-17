c7dc5a11eda872a6459e090dfbd03147047395d4
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-08-17 20:52:36 +0700

    A

--- components/Box.js
+++ components/Box.js
@@ -6,12 +6,21 @@ export default class Box extends PureComponent {
     constructor(props){
         super(props)
         this.state = {count : 0}
+        this.onCong = this.onCong.bind(this)
+        this.onTru = this.onTru.bind(this)
+    }
+    onCong(){
+        this.setState({count : this.state.count + 1})   
+    }
+    onTru(){
+        this.setState({count : this.state.count - 1})   
     }
     render() {
         return (
             <View style={{flex : 1 , flexDirection : "column" , justifyContent : "center" , alignItems : "center"}}>
                 <Text style={{fontSize : 30 , color : "red"}}> Value = {this.state.count}</Text>
-                <Child/>
+                <Child onCong={this.onCong}
+                onTru={this.onTru}/>
             </View>
         )
     }
--- components/Child.js
+++ components/Child.js
@@ -3,26 +3,29 @@ import { Text, View , TouchableOpacity} from 'react-native'
 
 export default class Child extends PureComponent {
     render() {
+        const {onCong , onTru} = this.props
         return (
             <View style={{flexDirection : "row"}}>
-                    <TouchableOpacity
+                <TouchableOpacity
+                        onPress={onCong}
                     >
                         <View style={{backgroundColor : "green" , padding : 5 , borderRadius : 5 }}>
                             <Text style={{color : "white", fontSize : 30}} >Cộng</Text>
                         </View>                                                
-                    </TouchableOpacity>
-                    <TouchableOpacity
+                </TouchableOpacity>
+                <TouchableOpacity
+                        onPress={onTru}
                     >
                         <View style={{backgroundColor : "red" , padding : 5 , borderRadius : 5 }}>
                             <Text style={{color : "white", fontSize : 30}} >Trừ</Text>
                         </View>                                                
-                    </TouchableOpacity>
-                    <TouchableOpacity
+                </TouchableOpacity>
+                <TouchableOpacity
                     >
                         <View style={{backgroundColor : "orange" , padding : 5 , borderRadius : 5 }}>
                             <Text style={{color : "white", fontSize : 30}} >Reset</Text>
                         </View>                                                
-                    </TouchableOpacity>
+                </TouchableOpacity>
             </View>
         )
     }
