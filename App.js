5a049b62ac1599a7d8169a3e76df4562b4372530
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-08-20 20:17:42 +0700

    A

--- App.js
+++ App.js
@@ -1,16 +1,31 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { createStore } from 'redux'
+
 import List from './components/List';
 import Form from './components/Form';
 import MyFlatlist from './components/MyFlatlist';
 import Filter from './components/Filter';
 import Box from './components/Box';
 
+let defaultState = {
+    count : 0
+}
+
+let store = createStore((state = defaultState , action) => {
+    if(action.type === "INCREASE") return {...state , count : state.count + 1}
+    return state
+})
+
+// dispatch : thay đổi giá trong store
+store.dispatch({type : "INCREASE"})
+//lay dư liệu của store
+alert(JSON.stringify(store.getState()))
 
 export default class App extends Component {
   render() {
     return (
-        <List/>
+        <Box/>
     );
   }
 }
