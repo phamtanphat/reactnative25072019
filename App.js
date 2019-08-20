aa0022008042cccf12004224a7fb9cbf00d9ea9f
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-08-20 20:36:38 +0700

    a

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
 
 import List from './components/List';
 import Form from './components/Form';
@@ -9,23 +10,26 @@ import Filter from './components/Filter';
 import Box from './components/Box';
 
 let defaultState = {
-    count : 0
+    count : 2
 }
 
 let store = createStore((state = defaultState , action) => {
     if(action.type === "INCREASE") return {...state , count : state.count + 1}
+    if(action.type === "DESCREASE") return {...state , count : state.count - 1}
+    if(action.type === "RESET") return {...state , count : 0}
     return state
 })
-
 // dispatch : thay đổi giá trong store
-store.dispatch({type : "INCREASE"})
+// store.dispatch({type : "INCREASE"})
 //lay dư liệu của store
-alert(JSON.stringify(store.getState()))
+// alert(JSON.stringify(store.getState()))
 
 export default class App extends Component {
   render() {
     return (
-        <Box/>
+      <Provider store={store}>
+          <Box/>
+      </Provider>
     );
   }
 }
--- components/Box.js
+++ components/Box.js
@@ -1,27 +1,17 @@
 import React, { PureComponent } from 'react'
 import { Text, View  } from 'react-native'
 import Child from './Child';
+import {connect} from 'react-redux'
 
-export default class Box extends PureComponent {
-    constructor(props){
-        super(props)
-        this.state = {count : 0}
-        this.onCong = this.onCong.bind(this)
-        this.onTru = this.onTru.bind(this)
-    }
-    onCong(){
-        this.setState({count : this.state.count + 1})   
-    }
-    onTru(){
-        this.setState({count : this.state.count - 1})   
-    }
+class Box extends PureComponent {
+
     render() {
         return (
             <View style={{flex : 1 , flexDirection : "column" , justifyContent : "center" , alignItems : "center"}}>
-                <Text style={{fontSize : 30 , color : "red"}}> Value = {this.state.count}</Text>
-                <Child onCong={this.onCong}
-                onTru={this.onTru}/>
+                <Text style={{fontSize : 30 , color : "red"}}> Value = {this.props.count}</Text>
+                <Child />
             </View>
         )
     }
 }
+export default connect(state => {return {count : state.count}})(Box)
